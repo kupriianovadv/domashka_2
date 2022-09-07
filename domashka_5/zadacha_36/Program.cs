096ad3d9ddf50aa67c54462c206ce8723ebd8721
Dariia <dashenka_korshun@mail.ru>
2022-09-07 18:50:53 +0300

    Save36

---
+++ domashka_5/zadacha_36/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] array = new int[4];
+int size = array.Length;
+
+// int count_chet = 0;
+// int count_nechet = 0;
+    
+for(int i=0;i<size;i++)
+{
+   =new Random().Next(100,1000);
+   if (array[i]%2>0)
+    {
+    array[i]++;
+    }
+}
+
+Console.WriteLine(String.Join("; ",array));
+Console.WriteLine("Количество четных чисел в массиве = " + count);
