79ee6074cf8f5313950fed650dae58ce18cfed63
Dariia <dashenka_korshun@mail.ru>
2022-09-16 16:17:21 +0300

    Save50

--- domashka_7/zadacha_47/Program.cs
+++ domashka_7/zadacha_47/Program.cs
@@ -4,17 +4,22 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+Console.WriteLine("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Массив: ");
 
-int [,] matrix =new int[rows, columns];
+double [,] matrix =new double[rows, columns];
 
 for(int i=0; i<matrix.GetLength(0); i++)
 {
     for(int j=0; j<matrix.GetLength(1); j++)
     {
-        matrix[i,j] = new Random().NextDouble(0,11);
-        Console.Write(matrix[i,j] + "\t");
+        matrix[i,j] = new Random().NextDouble()*(100);
+        Console.Write(Math.Round(matrix[i,j],2) + "\t");
     }
+    
     Console.WriteLine();
 }
---
+++ domashka_7/zadacha_50/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+
+double [,] matrix =new double[rows, columns];
+
+for(int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,11);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите номер строки элемента: ");
+int rowNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца элемента: ");
+int columnNum = Convert.ToInt32(Console.ReadLine());
+
+if(rows < rowNum || columns < columnNum)
+{
+  Console.WriteLine("Такой позиции в массиве нет");
+}
+else
+Console.WriteLine("Значение запрашиваемого элемента: " + matrix[rowNum-1, columnNum-1]);
