b6cb20ff31db03078ce28705c9e5917f01651536
Dariia <dashenka_korshun@mail.ru>
2022-09-21 16:15:18 +0300

    save_54&56

---
+++ domashka_8/zadacha_54/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+
+int [,] matrix =new int[rows, columns];
+
+for(int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(11);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Упорядоченный массив: ");
+for(int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        for (int k=0; k<matrix.GetLength(1)-1; k++)
+        {
+            if (matrix[i,k]<matrix[i,k+1])
+            {
+                int count = matrix[i,k];
+                matrix[i,k] = matrix[i,k+1];
+                matrix[i,k+1] = count;
+            }
+        }
+        Console.Write(matrix[i,j] + "\t");
+
+    }
+    Console.WriteLine();
+}
+
+
---
+++ domashka_8/zadacha_56/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int temp = int.MaxValue;
+
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+
+int [,] matrix =new int[rows, columns];
+
+if (rows != columns)
+{
+    for(int i=0; i<matrix.GetLength(0); i++)
+    {
+        for(int j=0; j<matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(11);
+            Console.Write(matrix[i,j] + "\t");
+        }
+    
+        Console.WriteLine();
+    }
+    Console.WriteLine("  ");
+
+    int k = 0;
+
+    for (int i = 0; i<matrix.GetLength(0); i++)
+    {
+        int sum  = 0;
+        for (int j = 0; j<matrix.GetLength(1); j++)
+        {
+            sum+=matrix[i,j];
+        }
+        Console.WriteLine($"Сумма элементов строки {i+1} = {sum}");
+        
+        if (temp>sum)
+        {
+            temp = sum;
+            k = i;
+        }
+    }
+        Console.WriteLine($"Номер строки с наименьшим количеством элементов {k+1}");
+}
+else{Console.WriteLine("Массив не прямоугольный");}
+
