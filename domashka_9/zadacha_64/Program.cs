e42f017020865cf32f373f693627152e1f2f5643
Dariia <dashenka_korshun@mail.ru>
2022-09-30 17:26:22 +0300

    saveomashka9

---
+++ domashka_9/zadacha_64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string NaturalNum (int M, int N)
+{
+    if (M<=N) return $"{M} "+ NaturalNum(M+1,N);
+    else return string.Empty;
+}
+Console.WriteLine(NaturalNum (m,n));
+
+string NaturalNumBack (int M, int N)
+{
+    if (M>=N) return $"{M} "+ NaturalNumBack(M-1,N);
+    else return string.Empty;
+}
+Console.WriteLine(NaturalNumBack (m,n));
---
+++ domashka_9/zadacha_66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+string SumNumber (int M, int N)
+{
+    if (M<=N) return (M + SumNumber(M+1,M)); 
+    else return string.Empty;
+}
+Console.WriteLine(SumNumber(m,n));
